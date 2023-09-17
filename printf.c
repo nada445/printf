6691cd22e28ab072be40a7ffbbb95bb6d65bdabf
mennaramadan9 <mennaramadan56@gmail.com>
2023-09-16 23:22:26 -0700

    i

--- printf.c
+++ printf.c
@@ -40,13 +40,14 @@ ops_t print[] = {
 {"c", print_char},
 {"s", print_str},
 {NULL, NULL}};
-int charcount = 0, i = 0;
+int charcount = 0;
 va_list ptr;
 va_start(ptr, format);
 if (!format || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 while (*format)
 {
+i = 0;
 if (*format != '%')
 {
 _putchar(*format);
