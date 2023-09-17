9ae55ac19f07fc177f790133f60ef99458f6fcfb
mennaramadan9 <mennaramadan56@gmail.com>
2023-09-17 01:15:16 -0700

    i

--- printf.c
+++ printf.c
@@ -27,47 +27,52 @@ _putchar(*str);
 str++;
 }
 }
+
 /**
- * print-int - does something
+ * print_int - does something
  * @ptr: variable
  * @charcountp: variable
+ * Return: void
  */
 void print_int(va_list ptr, int *charcountp)
 {
-int divisor = 1;
+int divisor, i;
 int num = va_arg(ptr, int);
-int num_copy, num_digits, i, digit;
+int num_copy = num;
+int num_digits = 0;
 
-if (ptr == NULL)
-return;
-if (num < 0)
-{
-_putchar('-');
-(*charcountp) += 1;
-num = -num; }
+do {
+num_copy /= 10;
+num_digits++;
+} while (num_copy != 0);
+
 if (num == 0)
 {
 _putchar('0');
-(*charcountp) += 1; }
-else
+(*charcountp) += 1;
+}
+if (num < 0)
 {
-num_copy = num;
-num_digits = 0;
+_putchar('-');
+*(charcountp) += 1;
+num = -num;
+}
 
-while (num_copy != 0)
-{
-num_copy /= 10;
-num_digits++; }
+divisor = 1;
 for (i = 1; i < num_digits; i++)
 {
-divisor *= 10; }
+divisor *= 10;
+}
+
 while (divisor > 0)
 {
-digit = num / divisor;
+int digit = num / divisor;
 _putchar('0' + digit);
-(*charcountp) += 1;
+*(charcountp) += 1;
 num %= divisor;
-divisor /= 10; }}}
+divisor /= 10;
+}
+}
 
 /**
  * _printf - printf function
