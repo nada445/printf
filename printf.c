f0d6f462daf6b72c21227cbc7a941bb2334f2b4d
mennaramadan9 <mennaramadan56@gmail.com>
2023-09-17 03:59:11 -0700

    int n

--- printf.c
+++ printf.c
@@ -38,28 +38,21 @@ void print_int(va_list ptr, int *charcountp)
 {
 int divisor, i;
 int num = va_arg(ptr, int);
-int num_copy = num;
-int num_digits = 0;
-
-do {
-num_copy /= 10;
-num_digits++;
-} while (num_copy != 0);
 
 if (num < 0)
 {
 _putchar('-');
-(*charcountp) += 1;
+(*charcountp)++;
 num = -num;
 }
 if (num == 0)
 {
 _putchar('0');
-(*charcountp) += 1;
+(*charcountp)++;
 }
 
 divisor = 1;
-for (i = 1; i < num_digits; i++)
+for (i = 1; i < num; i *= 10)
 {
 divisor *= 10;
 }
