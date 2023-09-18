0c3db780e2729a8ba5413de7511c834d220464b1
NADA <nadaelsharkawy545@gmail.com>
2023-09-18 12:46:37 -0700

    rot13

--- print_rot13.c
+++ print_rot13.c
@@ -1,38 +1,29 @@
 #include "main.h"
+
 /**
  * print_rot13 - prints the ROT13-encoded string
  * @ptr: va_list pointer
  * @charcountp: pointer to character count
  */
-
 void print_rot13(va_list ptr, int *charcountp)
 {
-int i;
-int j;
-char *str = va_arg(ptr, char *);
-char orarr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char charr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-if (!str)
+char *s = va_arg(ptr, char *);
+if (!s)
 {
-str = "(null)";
+s = "(null)";
+}
+for (int i = 0; s[i] != '\0'; i++)
+{
+char c = s[i];
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+char base = (c >= 'a' && c <= 'z') ? 'a' : 'A';
+_putchar(((c - base + 13) % 26) + base);
 }
 else
 {
-for (i = 0; str[i] != '\0'; i++)
-{
-for (j = 0; orarr[j] != '\0'; j++)
-{
-if (str[i] == orarr[j])
-{
-str[i] = charr[j];
-break;
+_putchar(c);
 }
-}
-}
-}
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
 (*charcountp) += 1;
 }
 }
